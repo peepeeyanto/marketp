d3e5185330801e21c87364dbf58fe0fd71e62ee4
unknown <doni.febrian20@mhs.uinjkt.ac.id>
2025-01-14 16:10:54 +0700

    changed the cart element in user dashboard to button

--- resources/views/frontend/dashboard/layouts/navbar.blade.php
+++ resources/views/frontend/dashboard/layouts/navbar.blade.php
@@ -2,7 +2,8 @@
     <div class="wsusd__dashboard_user">
       {{-- <img src="images/dashboard_user.jpg" alt="img" class="img-fluid"> --}}
       <p class="me-4">{{ Auth::user()->name }}</p>
-      <i class="fas fa-cart-plus me-2"></i>
-      <a href="{{route('cart-detail')}}">{{Cart::content()->count() }} </a>
+      <a href="{{route('cart-detail')}}" class="btn btn-warning"><span class="fas fa-cart-plus me-2"></span>{{ Cart::content()->count() }}</a>
+      {{-- <i class="fas fa-cart-plus me-2"></i>
+      <a href="{{route('cart-detail')}}">{{Cart::content()->count() }} </a> --}}
     </div>
   </div>
